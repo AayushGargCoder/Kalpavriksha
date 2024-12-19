0cf9870f6fe12371d8bbc59d215bfa5974a5524f
Aayush Garg <gargaayush32@gmail.com>
2024-12-19 15:13:16 +0530

    Assignment-1:Improve code readibility

--- 01-calculator.c
+++ 01-calculator.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdbool.h>
 
 #define MAX 101
+char operatorsAllowed[] = "/*+-";
+char expression[MAX];
 // use typedef  so that no write to use struct everytime while creating stack structure
 typedef struct
 {
@@ -53,14 +53,25 @@ int applyOperation(int a, int b, char op)
         return a / b;
     }
 }
-char operatorsAllowed[] = "/*+-";
 
+char *findElement(char inp)
+{
+    for (size_t i = 0; i < 4; i++)
+    {
+        if (operatorsAllowed[i] == inp)
+            return operatorsAllowed + i;
+    }
+}
+bool isDigit(char inp)
+{
+    return (inp >= '0' && inp <= '9');
+}
 // This function check for invalid input like alphabest,special characters and return true if found
 bool invalidInput(char inp)
 {
-    if (strchr(operatorsAllowed, inp) == NULL && !isdigit(inp))
-        return true;
-    return false;
+    if (findElement(inp) != NULL || isDigit(inp))
+        return false;
+    return true;
 }
 
 // This function pop two operators and operand and evaluate
@@ -72,43 +83,52 @@ void evaluate(Stack *operands, Stack *operators)
     push(operands, applyOperation(a, b, op));
 }
 
-int evaluateExpression(const char *expression)
+size_t strlen(const char *str)
+{
+    size_t len = 0;
+    while (str[len] != '\0')
+        len++;
+    return len;
+}
+
+int evaluateExpression()
 {
     Stack operators, operands;
     operands.topIndex = operators.topIndex = -1;
     // expression always starts with an operand.
     bool operandTurn = true;
-    int i = 0, n = strlen(expression);
-    while (i < n)
+    int currPos = 0;
+    size_t expressionLen = strlen(expression);
+    while (currPos < expressionLen)
     {
-        if (isspace(expression[i]))
-            i++;
-        else if (invalidInput(expression[i]))
+        if (expression[currPos] == ' ')
+            currPos++;
+        else if (invalidInput(expression[currPos]))
             Error("Error:Invalid Input");
         // If the input is a number,parse it to integer
         // if input->+3+4 and operandTurn is true but first input is operator, means input is invalid
-        else if (isdigit(expression[i]) && operandTurn)
+        else if (isDigit(expression[currPos]) && operandTurn)
         {
             int value = 0;
-            while (i < n && isdigit(expression[i]))
+            while (currPos < expressionLen && isDigit(expression[currPos]))
             {
-                value = value * 10 + (expression[i] - '0');
-                i++;
+                value = value * 10 + (expression[currPos] - '0');
+                currPos++;
             }
             push(&operands, value);
             operandTurn = false;
         }
         // if the input is an operator
-        else if (strchr(operatorsAllowed, expression[i]) != NULL && !operandTurn)
+        else if (findElement(expression[currPos]) != NULL && !operandTurn)
         {
             // if input->12++45 then after examine 12+ operantTurn=false and again when found + but operandTurn is true,means expression is invalid
 
             //  While top of operators has the same or greater precedence
-            while (operators.topIndex != -1 && precedence(top(&operators)) >= precedence(expression[i]))
+            while (operators.topIndex != -1 && precedence(top(&operators)) >= precedence(expression[currPos]))
                 evaluate(&operands, &operators);
-            push(&operators, expression[i]);
+            push(&operators, expression[currPos]);
             operandTurn = true;
-            i++;
+            currPos++;
         }
         else
             Error("Error:Invalid Input");
@@ -126,17 +146,31 @@ int evaluateExpression(const char *expression)
     return pop(&operands);
 }
 
-int main()
+void removeNewLine()
 {
+    for (int i = 0; expression[i] != '\0'; i++)
+    {
+        if (expression[i] == '\n')
+        {
+            expression[i] = '\0';
+            return;
+        }
+    }
+}
 
+void takeInput()
+{
     printf("Enter a mathematical expression : ");
-    char expression[MAX];
     // fgets read upto newline or n-1 character read from standared input(keyboard) and store it into expression
     fgets(expression, MAX, stdin);
     // fgets read new Line char also,  so place null char there if found.
-    // strcspn->find no of characters before \n,means provide index of \n.
-    expression[strcspn(expression, "\n")] = '\0';
-    int result = evaluateExpression(expression);
+    removeNewLine();
+}
+int main()
+{
+
+    takeInput();
+    int result = evaluateExpression();
     printf("Result: %d\n", result);
 
     return 0;
