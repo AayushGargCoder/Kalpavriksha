f2f7cf035e8de54cae8e648a93f0e8f9aa228c0b
Aayush Garg <gargaayush32@gmail.com>
2025-01-04 14:16:03 +0530

    exponentiation-evaluation-pointer

--- 03-exprssionEvaluation.c
+++ 03-exprssionEvaluation.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
-int base, expo, m;
-void binaryExpo()
+void binaryExpo(int *ptrBase, int *ptrExpo, int *ptrM)
 {
     int res = 1;
-    while (expo)
+    while (*ptrExpo)
     {
-        if (expo & 1)
-            res = res * 1LL * base % m;
-        base = base * 1LL * base % m;
-        expo = expo / 2;
+        if (*ptrExpo & 1)
+            res = res * 1LL * (*ptrBase) % (*ptrM);
+        *ptrBase = (*ptrBase) * 1LL * (*ptrBase) % (*ptrM);
+        *ptrExpo = (*ptrExpo) / 2;
     }
     printf("\n\nOutput:\n");
     printf("Result: %d", res);
 }
-int main()
+void takeInput(int *ptrBase, int *ptrExpo, int *ptrM)
 {
     printf("Input:\n");
     printf("Base (B): ");
-    scanf("%d", &base);
-    getchar();
+    scanf("%d", ptrBase);
     // Prompt and input for exponent
     printf("Exponent (N): ");
-    scanf("%d", &expo);
-    getchar();
+    scanf("%d", ptrExpo);
 
     // Prompt and input for modulus
     printf("Modulus (M): ");
-    scanf("%d", &m);
-    if (expo >= 0 && m > 1)
-        binaryExpo();
+    scanf("%d", ptrM);
+}
+int main()
+{
+    int base, expo, m;
+    takeInput(&base, &expo, &m);
+    if (expo >= 0 && m > 1 && base > 0)
+        binaryExpo(&base, &expo, &m);
     else
     {
         printf("Please enter a valid input");
